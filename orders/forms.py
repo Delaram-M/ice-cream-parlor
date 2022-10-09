d8cfb30a2b8ccb45a84d1ad8841763b23ed78688
Delaram.M <delaram.moradi@gmail.com>
2022-10-09 21:29:58 +0330

    change form to work with prep duration model

--- orders/forms.py
+++ orders/forms.py
@@ -1,12 +1,7 @@
-import datetime
-
 from django.forms import ModelForm, ChoiceField, SelectMultiple
 from django.utils import timezone
 
 from orders.models import Order, Flavor, PrepDuration
-
-# TODO move to DB
-PREP_CHOICES = [15, 30, 60]
 
 
 class OrderForm(ModelForm):
@@ -17,7 +12,7 @@ class OrderForm(ModelForm):
             'flavors': SelectMultiple(attrs={'size': len(Flavor.objects.all())}),
         }
 
-    prep_durations = PrepDuration.objects.all()
-    pickup_choices = [timezone.now() + datetime.timedelta(minutes=delta) for delta in PREP_CHOICES]
+    prep_durations = PrepDuration.objects.values_list('duration', flat=True)
+    pickup_choices = [timezone.now() + delta for delta in prep_durations]
     pickup_tuple = tuple(zip(pickup_choices, [choice.strftime("%m/%d, %H:%M") for choice in pickup_choices]))
     pickup_time = ChoiceField(choices=pickup_tuple)
