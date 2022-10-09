1763c0118505e32bf16fa5e9e1ea4862c1b0df91
Delaram.M <delaram.moradi@gmail.com>
2022-10-09 21:01:20 +0330

    make url paths unique

--- ice_cream_parlor/urls.py
+++ ice_cream_parlor/urls.py
@@ -3,7 +3,6 @@ from django.urls import include
 from django.urls import path
 
 urlpatterns = [
-    path('', include('orders.urls')),
     path('orders/', include('orders.urls')),
     path('admin/', admin.site.urls),
 ]
