c87e11eae3900f6badb2c801def1d060edb69c48
Delaram.M <delaram.moradi@gmail.com>
2022-10-13 21:40:59 +0330

    add order info to receipt

--- orders/views.py
+++ orders/views.py
@@ -13,6 +13,17 @@ class OrderCreateView(CreateView):
     model = Order
     form_class = OrderForm
 
+    def form_valid(self, form):
+        response = super(OrderCreateView, self).form_valid(form)
+        self.request.session['customer_name'] = str(form.cleaned_data['customer_name'])
+        self.request.session['flavors'] = str([flavor.name for flavor in form.cleaned_data['flavors']])[1:-1]
+        self.request.session['pickup_time'] = str(form.cleaned_data['pickup_time'])[5:16]
+        return response
+
 
 def receipt(request):
-    return render(request, 'orders/receipt.html')
+    receipt_data = dict()
+    for key, value in request.session.items():
+        receipt_data[key] = value
+    context = {'receipt_data': receipt_data}
+    return render(request, 'orders/receipt.html', context)
